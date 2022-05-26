5b4948b6964434deb1f633079339f9adbf56eb6a
Kwitsch <kwitsch@gmail.com>
2022-05-26 23:16:02 +0200

    UsersCurrent with anonymous sub structs

--- model/users.go
+++ model/users.go
@@ -1,29 +1,25 @@
 package model
 
 type UsersCurrent struct {
-	Id        string                `json:"id"`
-	Type      int                   `json:"type"`
-	RoleType  int                   `json:"roleType"`
-	Privilege UsersCurrentPrivilege `json:"privilege"`
-	Adopt     bool                  `json:"adopt"`
-	Manage    bool                  `json:"manage"`
-	License   bool                  `json:"license"`
-	Name      string                `json:"name"`
-	Email     string                `json:"email"`
-	Alert     bool                  `json:"alert"`
-	Disaster  int                   `json:"disaster"`
-	Favorites []string              `json:"favorites"`
-	Dbnormal  bool                  `json:"dbnormal"`
-}
-
-type UsersCurrentPrivilege struct {
-	All         bool                        `json:"all"`
-	LastVisited string                      `json:"lastVisited"`
-	Sites       []UsersCurrentPrivilegeSite `json:"sites"`
-}
-
-type UsersCurrentPrivilegeSite struct {
-	Key     string `json:"key"`
-	Name    string `json:"name"`
-	Primary bool   `json:"primary"`
+	Id        string `json:"id"`
+	Type      int    `json:"type"`
+	RoleType  int    `json:"roleType"`
+	Privilege struct {
+		All         bool   `json:"all"`
+		LastVisited string `json:"lastVisited"`
+		Sites       []struct {
+			Key     string `json:"key"`
+			Name    string `json:"name"`
+			Primary bool   `json:"primary"`
+		} `json:"sites"`
+	} `json:"privilege"`
+	Adopt     bool     `json:"adopt"`
+	Manage    bool     `json:"manage"`
+	License   bool     `json:"license"`
+	Name      string   `json:"name"`
+	Email     string   `json:"email"`
+	Alert     bool     `json:"alert"`
+	Disaster  int      `json:"disaster"`
+	Favorites []string `json:"favorites"`
+	Dbnormal  bool     `json:"dbnormal"`
 }
