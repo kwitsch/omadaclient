bab29498004b0c2b34816e5dd9a82ce688539cf5
Kwitsch <kwitsch@gmail.com>
2023-05-20 22:45:26 +0200

    extracted inline structs

--- model/network.go
+++ model/network.go
@@ -5,39 +5,45 @@ type Networks struct {
 	Data []Network `json:"data"`
 }
 
+type NetworkDhcpGuard struct {
+	Enable   bool   `json:"enable"`
+	DhcpSvr1 string `json:"dhcpSvr1"`
+}
+
+type NetworkDhcpSettings struct {
+	Enable       bool   `json:"enable"`
+	IpaddrStart  string `json:"ipaddrStart"`
+	IpaddrEnd    string `json:"ipaddrEnd"`
+	IPRangeStart int64  `json:"ipRangeStart"`
+	IPRangeEnd   int64  `json:"ipRangeEnd"`
+	Dhcpns       string `json:"dhcpns"`
+	PriDNS       string `json:"priDns"`
+	SndDNS       string `json:"sndDns"`
+	Leasetime    uint64 `json:"leasetime"`
+	Option138    string `json:"option138"`
+}
+
+type NetworkLanNetworkIpv6Config struct {
+	Proto  string `json:"proto"`
+	Enable int    `json:"enable"`
+}
+
 type Network struct {
-	ID                string   `json:"id"`
-	Site              string   `json:"site"`
-	Name              string   `json:"name"`
-	Purpose           string   `json:"purpose"`
-	InterfaceIds      []string `json:"interfaceIds"`
-	Vlan              uint64   `json:"vlan"`
-	GatewaySubnet     string   `json:"gatewaySubnet"`
-	Domain            string   `json:"domain,omitempty"`
-	IgmpSnoopEnable   bool     `json:"igmpSnoopEnable"`
-	Portal            bool     `json:"portal"`
-	AccessControlRule bool     `json:"accessControlRule"`
-	RateLimit         bool     `json:"rateLimit"`
-	AllLan            bool     `json:"allLan"`
-	Primary           bool     `json:"primary"`
-	DhcpGuard         struct {
-		Enable   bool   `json:"enable"`
-		DhcpSvr1 string `json:"dhcpSvr1"`
-	} `json:"dhcpGuard,omitempty"`
-	DhcpSettings struct {
-		Enable       bool   `json:"enable"`
-		IpaddrStart  string `json:"ipaddrStart"`
-		IpaddrEnd    string `json:"ipaddrEnd"`
-		IPRangeStart int64  `json:"ipRangeStart"`
-		IPRangeEnd   int64  `json:"ipRangeEnd"`
-		Dhcpns       string `json:"dhcpns"`
-		PriDNS       string `json:"priDns"`
-		SndDNS       string `json:"sndDns"`
-		Leasetime    uint64 `json:"leasetime"`
-		Option138    string `json:"option138"`
-	} `json:"dhcpSettings,omitempty"`
-	LanNetworkIpv6Config struct {
-		Proto  string `json:"proto"`
-		Enable int    `json:"enable"`
-	} `json:"lanNetworkIpv6Config,omitempty"`
+	ID                   string                      `json:"id"`
+	Site                 string                      `json:"site"`
+	Name                 string                      `json:"name"`
+	Purpose              string                      `json:"purpose"`
+	InterfaceIds         []string                    `json:"interfaceIds"`
+	Vlan                 uint64                      `json:"vlan"`
+	GatewaySubnet        string                      `json:"gatewaySubnet"`
+	Domain               string                      `json:"domain,omitempty"`
+	IgmpSnoopEnable      bool                        `json:"igmpSnoopEnable"`
+	Portal               bool                        `json:"portal"`
+	AccessControlRule    bool                        `json:"accessControlRule"`
+	RateLimit            bool                        `json:"rateLimit"`
+	AllLan               bool                        `json:"allLan"`
+	Primary              bool                        `json:"primary"`
+	DhcpGuard            NetworkDhcpGuard            `json:"dhcpGuard,omitempty"`
+	DhcpSettings         NetworkDhcpSettings         `json:"dhcpSettings,omitempty"`
+	LanNetworkIpv6Config NetworkLanNetworkIpv6Config `json:"lanNetworkIpv6Config,omitempty"`
 }
--- model/users.go
+++ model/users.go
@@ -1,25 +1,29 @@
 package model
 
+type UserPrivilege struct {
+	All         bool            `json:"all"`
+	LastVisited string          `json:"lastVisited"`
+	Sites       []PrivilegeSite `json:"sites"`
+}
+
+type PrivilegeSite struct {
+	Key     string `json:"key"`
+	Name    string `json:"name"`
+	Primary bool   `json:"primary"`
+}
+
 type UsersCurrent struct {
-	Id        string `json:"id"`
-	Type      uint64 `json:"type"`
-	RoleType  uint64 `json:"roleType"`
-	Privilege struct {
-		All         bool   `json:"all"`
-		LastVisited string `json:"lastVisited"`
-		Sites       []struct {
-			Key     string `json:"key"`
-			Name    string `json:"name"`
-			Primary bool   `json:"primary"`
-		} `json:"sites"`
-	} `json:"privilege"`
-	Adopt     bool     `json:"adopt"`
-	Manage    bool     `json:"manage"`
-	License   bool     `json:"license"`
-	Name      string   `json:"name"`
-	Email     string   `json:"email"`
-	Alert     bool     `json:"alert"`
-	Disaster  int      `json:"disaster"`
-	Favorites []string `json:"favorites"`
-	Dbnormal  bool     `json:"dbnormal"`
+	Id        string        `json:"id"`
+	Type      uint64        `json:"type"`
+	RoleType  uint64        `json:"roleType"`
+	Privilege UserPrivilege `json:"privilege"`
+	Adopt     bool          `json:"adopt"`
+	Manage    bool          `json:"manage"`
+	License   bool          `json:"license"`
+	Name      string        `json:"name"`
+	Email     string        `json:"email"`
+	Alert     bool          `json:"alert"`
+	Disaster  int           `json:"disaster"`
+	Favorites []string      `json:"favorites"`
+	Dbnormal  bool          `json:"dbnormal"`
 }
